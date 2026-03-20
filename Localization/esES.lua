51c49556611f1d9d241da3b8ed17be03d08e2e97
Michael Jones <25686778+mikeacjones@users.noreply.github.com>
2026-03-20 14:42:42 -0400

    Add French and Spanish localizations

---
+++ Localization/esES.lua
@@ -0,0 +1,17 @@
+local _, ts = ...
+if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+
+ts.L.TOGGLE = "Mostrar/ocultar ventana del Planificador de Talentos"
+ts.L.MANAGE = "Gestionar"
+ts.L.IMPORT = "Importar URL"
+ts.L.IMPORT_DIALOG = "Pega una URL de talentos de Wowhead en el cuadro de abajo"
+ts.L.OK = "Aceptar"
+ts.L.CANCEL = "Cancelar"
+ts.L.WRONG_CLASS = "No se puede importar, ¡no eres un %s!"
+ts.L.LOAD_SEQUENCE_TIP = "Haz clic para cargar la secuencia"
+ts.L.LOAD_OTHER_CLASS_TIP = "Solo se pueden cargar secuencias de tu clase actual"
+ts.L.DELETE_TIP = "<Mayús>Clic para eliminar"
+ts.L.RENAME_TIP = "Haz clic para renombrar"
+ts.L.CURRENT_CLASS_LABEL = "Personaje actual"
+ts.L.UNNAMED = "<sin nombre>"
+ts.L.NO_ORDER = "Esta URL no incluye un orden de talentos. Activa \"Orden de talentos\" en la calculadora de Wowhead antes de copiar la URL."
---
+++ Localization/frFR.lua
@@ -0,0 +1,17 @@
+local _, ts = ...
+if GetLocale() ~= "frFR" then return end
+
+ts.L.TOGGLE = "Basculer la fenêtre du Planificateur de Talents"
+ts.L.MANAGE = "Gérer"
+ts.L.IMPORT = "Importer URL"
+ts.L.IMPORT_DIALOG = "Collez une URL de talents Wowhead dans la case ci-dessous"
+ts.L.OK = "OK"
+ts.L.CANCEL = "Annuler"
+ts.L.WRONG_CLASS = "Impossible d'importer, vous n'êtes pas un %s !"
+ts.L.LOAD_SEQUENCE_TIP = "Cliquez pour charger la séquence"
+ts.L.LOAD_OTHER_CLASS_TIP = "Seules les séquences de votre classe actuelle peuvent être chargées"
+ts.L.DELETE_TIP = "<Maj>Cliquez pour supprimer"
+ts.L.RENAME_TIP = "Cliquez pour renommer"
+ts.L.CURRENT_CLASS_LABEL = "Personnage actuel"
+ts.L.UNNAMED = "<sans nom>"
+ts.L.NO_ORDER = "Cette URL n'inclut pas d'ordre de talents. Activez \"Ordre des talents\" dans le calculateur Wowhead avant de copier l'URL."
