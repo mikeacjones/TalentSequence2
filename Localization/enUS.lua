77c7594c9affb558ab493d606223b4e4a730f99d
Michael Jones <25686778+mikeacjones@users.noreply.github.com>
2026-03-20 14:40:35 -0400

    Move localization to per-language files under Localization/
    Replaces the monolithic Localization.lua with Localization/enUS.lua
    as the base. Additional languages can be added as separate files
    (e.g. deDE.lua, frFR.lua) that override individual keys.

---
+++ Localization/enUS.lua
@@ -0,0 +1,19 @@
+local _, ts = ...
+
+ts.L = {
+    TOGGLE = "Toggle Talent Planner Window",
+    LOAD = "Load",
+    MANAGE = "Manage",
+    IMPORT = "Import URL",
+    IMPORT_DIALOG = "Paste a Wowhead talent URL into the box below",
+    OK = "OK",
+    CANCEL = "Cancel",
+    WRONG_CLASS = "Unable to import, you're not a %s!",
+    LOAD_SEQUENCE_TIP = "Click to Load Sequence",
+    LOAD_OTHER_CLASS_TIP = "Only sequences for your current class can be loaded",
+    DELETE_TIP = "<Shift>Click to Delete",
+    RENAME_TIP = "Click to Rename",
+    CURRENT_CLASS_LABEL = "Current Character",
+    UNNAMED = "<unnamed>",
+    NO_ORDER = "This URL doesn't include a talent order. Enable \"Talent Order\" in the Wowhead calculator before copying the URL.",
+}
